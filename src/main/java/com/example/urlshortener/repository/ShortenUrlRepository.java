0b92792baeb70808884712aae12c24370f48d1e1
Serhii Yevtushok <ggazila@gmail.com>
2023-04-17 19:53:22 +0300

    url shortener redis implementation.

--- src/main/java/com/example/urlshortener/repository/ShortenUrlRepository.java
+++ src/main/java/com/example/urlshortener/repository/ShortenUrlRepository.java
@@ -1,7 +1,9 @@
 package com.example.urlshortener.repository;
 
+
 import com.example.urlshortener.entity.ShortenedUrl;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ShortenUrlRepository extends CrudRepository<ShortenedUrl, String> {
+
 }
